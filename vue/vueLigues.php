827c03d80c75f68c1110f5d4e5f88d51821c7fdd
Enguss <mrengus@gmail.com>
2021-09-17 16:08:17 +0200

    Ligue Image

--- vue/vueLigues.php
+++ vue/vueLigues.php
@@ -6,7 +6,7 @@
 		<img src="images/Bordeaux.png" alt="Logo Bordeaux">
 		<a class="btn btn-primary" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Voir tout les club de Bordeaux</a>
 
-		<img src="images/Bobigny.png" alt="Logo Bobigny">
+		<img src="images/Bobigny.jpg" alt="Logo Bobigny">
 		<a class="btn btn-primary" href="http://localhost/code/controleurLigues?m2lMP=Bobigny" role="button">Voir tout les club de Bobigny</a>
 
 	</main>
