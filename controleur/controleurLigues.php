0160d4bb1b97d000f3f75f2c80d884524b08237e
Enguss <mrengus@gmail.com>
2021-09-17 16:07:38 +0200

    Ligue

--- controleur/controleurLigues.php
+++ controleur/controleurLigues.php
@@ -1,18 +1,2 @@
-
-
-
-
-	// $formulaireLigue = new Formulaire('post', 'index.php', 'fLigue', 'fLigue');
-  //
-	// $formulaireLigue->ajouterComposantLigne($formulaireLigue->creerLabel('Ligue de Bordeaux'));
-	// $formulaireLigue->ajouterComposantLigne($formulaireLigue->creerInputImage('Image', 'Image', 'images/Bordeaux.png'));
-	// $formulaireLigue->ajouterComposantTab();
-  //
-  // $formulaireLigue->ajouterComposantLigne($formulaireLigue->creerLabel('Ligue de Bobigny'));
-  // $formulaireLigue->ajouterComposantLigne($formulaireLigue->creerInputImage('Image', 'Image', 'images/Bobigny.jpg'));
-  // $formulaireLigue->ajouterComposantTab();
-  //
-  //
-	// $formulaireLigue->creerFormulaire();
 <?php
 	require_once 'vue/vueLigues.php' ;
--- vue/vueLigues.php
+++ vue/vueLigues.php
@@ -5,6 +5,10 @@
 	<main>
 		<img src="images/Bordeaux.png" alt="Logo Bordeaux">
 		<a class="btn btn-primary" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Voir tout les club de Bordeaux</a>
+
+		<img src="images/Bobigny.png" alt="Logo Bobigny">
+		<a class="btn btn-primary" href="http://localhost/code/controleurLigues?m2lMP=Bobigny" role="button">Voir tout les club de Bobigny</a>
+
 	</main>
 	<footer>
 		<?php include 'bas.php' ;?>
