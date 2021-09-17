c31a48cb8d5443c2553b3cfbcdb27d523a50de40
Enguss <mrengus@gmail.com>
2021-09-17 16:47:20 +0200

    Modif a finir

--- controleur/controleurLigues.php
+++ controleur/controleurLigues.php
@@ -1,2 +1,8 @@
 <?php
+  require_once 'modele/dao/DAOLigues.php';
+
+  $ListeLigue = new Ligue();
+  $tabLigue = $ListeLigue->Ligue();
+
+  echo var_dump(tabLigue);
 	require_once 'vue/vueLigues.php' ;
--- vue/vueLigues.php
+++ vue/vueLigues.php
@@ -3,11 +3,9 @@
 		<?php include 'haut.php' ;?>
 	</header>
 	<main>
-		<img src="images/Bordeaux.png" alt="Logo Bordeaux">
-		<a class="btn btn-primary" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Voir tout les club de Bordeaux</a>
+		<a class="btn btn-primary" id='boutonbordeaux' href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Voir tout les club de Bordeaux</a>
 
-		<img src="images/Bobigny.jpg" alt="Logo Bobigny">
-		<a class="btn btn-primary" href="http://localhost/code/controleurLigues?m2lMP=Bobigny" role="button">Voir tout les club de Bobigny</a>
+		<a class="btn btn-primary" id='boutonbobigny' href="http://localhost/code/controleurLigues?m2lMP=Bobigny" role="button">Voir tout les club de Bobigny</a>
 
 	</main>
 	<footer>
