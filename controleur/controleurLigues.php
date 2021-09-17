807e0ee1a85238e107ae0feb4358085529dc33e7
Enguss <mrengus@gmail.com>
2021-09-17 16:02:45 +0200

    Vue Ligue Bordeaux

--- controleur/controleurLigues.php
+++ controleur/controleurLigues.php
@@ -1,14 +1,18 @@
+
+
+
+
+	// $formulaireLigue = new Formulaire('post', 'index.php', 'fLigue', 'fLigue');
+  //
+	// $formulaireLigue->ajouterComposantLigne($formulaireLigue->creerLabel('Ligue de Bordeaux'));
+	// $formulaireLigue->ajouterComposantLigne($formulaireLigue->creerInputImage('Image', 'Image', 'images/Bordeaux.png'));
+	// $formulaireLigue->ajouterComposantTab();
+  //
+  // $formulaireLigue->ajouterComposantLigne($formulaireLigue->creerLabel('Ligue de Bobigny'));
+  // $formulaireLigue->ajouterComposantLigne($formulaireLigue->creerInputImage('Image', 'Image', 'images/Bobigny.jpg'));
+  // $formulaireLigue->ajouterComposantTab();
+  //
+  //
+	// $formulaireLigue->creerFormulaire();
 <?php
-
-
-
-	$formulaireLigue = new Formulaire('post', 'index.php', 'fLigue', 'fLigue');
-
-	$formulaireLigue->ajouterComposantLigne($formulaireLigue->creerLabel('Ligue de Bordeaux'));
-	$formulaireLigue->ajouterComposantLigne($formulaireLigue->creerInputImage('Image', 'Image', 'images/lorraine.gif'));
-	$formulaireLigue->ajouterComposantTab();
-
-
-	$formulaireLigue->creerFormulaire();
-
 	require_once 'vue/vueLigues.php' ;
--- vue/vueLigues.php
+++ vue/vueLigues.php
@@ -3,9 +3,8 @@
 		<?php include 'haut.php' ;?>
 	</header>
 	<main>
-		<?php
-			$formulaireLigue->afficherFormulaire();
-		?>
+		<img src="images/Bordeaux.png" alt="Logo Bordeaux">
+		<a class="btn btn-primary" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Voir tout les club de Bordeaux</a>
 	</main>
 	<footer>
 		<?php include 'bas.php' ;?>
