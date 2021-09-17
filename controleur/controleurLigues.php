63ae690fdb6d9f78cc78a938c029dd036133b9b8
Enguss <mrengus@gmail.com>
2021-09-17 14:37:07 +0200

    ajout Formulaire Ligues

--- controleur/controleurLigues.php
+++ controleur/controleurLigues.php
@@ -1,4 +1,16 @@
 <?php
 
+if(!$_SESSION['identification']){
 
-require_once 'vue/vueLigues.php' ;
+	$formulaireLigue = new Formulaire('post', 'index.php', 'fLigue', 'fLigue');
+
+	$formulaireLigue->ajouterComposantLigne($formulaireLigue->creerLabel('Identifiant :'));
+	$formulaireLigue->ajouterComposantLigne($formulaireLigue->creerInputImage('Image', 'Image', 'images/lorraine.gif'));
+	$formulaireLigue->ajouterComposantTab();
+
+
+	$formulaireLigue->creerFormulaire();
+
+	require_once 'vue/vueLigue.php' ;
+
+}
