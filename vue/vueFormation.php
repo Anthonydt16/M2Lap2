2d12e0669613d7135579f1b3b31740e3c4019495
hugoLasserre <hugo.lasserrecab@gmail.com>
2021-09-24 13:58:45 +0200

    hugo

--- vue/vueFormation.php
+++ vue/vueFormation.php
@@ -3,7 +3,12 @@
 		<?php include 'haut.php' ;?>
 	</header>
 	<main>
-		
+		<div class = center-div>
+			<a class="btnFormation" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Formation n°1</a>
+			<a class="btnFormation" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Formation n°2</a>
+			<a class="btnFormation" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Formation n°3</a>
+			<a class="btnFormation" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Formation n°4</a>
+		</div>
 	</main>
 	<footer>
 		<?php include 'bas.php' ;?>
