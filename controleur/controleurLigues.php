f26e198201f2fb0eaafb17c63c4d5737c46e10ab
Enguss <mrengus@gmail.com>
2021-09-24 13:45:55 +0200

    Bouton Ligue Automatique

--- controleur/controleurLigues.php
+++ controleur/controleurLigues.php
@@ -4,5 +4,5 @@
   $ListeLigue = new Ligue();
   $tabLigue = $ListeLigue->Ligue();
 
-  echo var_dump(tabLigue);
+  echo var_dump($tabLigue);
 	require_once 'vue/vueLigues.php' ;
--- vue/vueLigues.php
+++ vue/vueLigues.php
@@ -3,9 +3,15 @@
 		<?php include 'haut.php' ;?>
 	</header>
 	<main>
-		<a class="btn btn-primary" id='boutonbordeaux' href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Voir tout les club de Bordeaux</a>
 
-		<a class="btn btn-primary" id='boutonbobigny' href="http://localhost/code/controleurLigues?m2lMP=Bobigny" role="button">Voir tout les club de Bobigny</a>
+		<?php
+
+			foreach ($tabLigue as $key){
+				echo '<a class="btn btn-primary" id= href="http://localhost/code/controleurLigues?m2lMP='.$key['nomLigue'].' role="button">Voir tout les club de '.$key['nomLigue'].'</a>';
+		}
+
+		?>
+
 
 	</main>
 	<footer>
