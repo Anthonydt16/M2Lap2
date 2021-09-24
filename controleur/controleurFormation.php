cae3905698dde33e138d524076fc4a0752798c38
hugoLasserre <hugo.lasserrecab@gmail.com>
2021-09-24 16:59:30 +0200

    hugo

--- controleur/controleurFormation.php
+++ controleur/controleurFormation.php
@@ -1,2 +1,9 @@
 <?php
+require_once 'modele/dao/DAOFormation.php';
+
+$uneFormation = new FormationDAO();
+$tabFormation = $uneFormation->getformation();
+
+/*echo var_dump($tabFormation);*/
+
 require_once 'vue/vueFormation.php' ;
--- vue/vueFormation.php
+++ vue/vueFormation.php
@@ -4,10 +4,33 @@
 	</header>
 	<main>
 		<div class = center-div>
-			<a class="btnFormation" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Formation n°1</a>
-			<a class="btnFormation" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Formation n°2</a>
-			<a class="btnFormation" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Formation n°3</a>
-			<a class="btnFormation" href="http://localhost/code/controleurLigues?m2lMP=Bordeaux" role="button">Formation n°4</a>
+			<?php
+			//fonction qui s'incrémente quand on entre dans la condition d'affichage pour ne pas afficher le message d'erreur même si on y entre
+			$testCondition=0;
+			foreach ($tabFormation as $key){
+				//si la date d'ouverture des inscriptions est anterieure à la date du jour, et que la date de fermeture est après la date du jour, Ou que la date de cloture est après à la date du jour, alors afficher les différentes formations
+				if($key['dateOuvertinscriptions']<date("Y-m-d") && $key['dateClotureInscriptions']<date("Y-m-d") || $key['dateOuvertinscriptions']>date("Y-m-d"))
+				{
+					echo '<div class="divBtnForma">';
+					echo '<p class=texteFormation>'.$key['intitule'].'</p>';
+					echo '<p class=texteFormation>'.$key['descriptif'].'</p>';
+					echo '<p class=texteFormation>'.'Durée : '.$key['duree'].'</p>';
+					echo '<p class=texteFormation>'.'Ouverture inscriptions : '.$key['dateOuvertinscriptions'].'</p>';
+					echo '<p class=texteFormation>'.'Fermeture inscriptions : '.$key['dateClotureInscriptions'].'</p>';
+					echo '<p class=texteFormation>'.'Déput Formation : '.$key['DateDebutFormation'].'</p>';
+					echo '<p class=texteFormation>'.'Effectif Max : '.$key['EffectifMax'].'</p>';
+					echo '<a class="btnFormation" href=""'.' role="button">S'."'".'inscrire'.'</a>';
+					echo '</div>';
+				}
+				else{
+					$testCondition++;
+				}
+			}
+			//permet de montrer à l'utilisateur s'il n'y a pas de futures formations ou de formations en cours
+			if(!$testCondition==0){
+				echo '<p>'.'Il n'."'".'y a pas de nouvelles formations pour le moment !'.'</p>';
+			}
+			?>
 		</div>
 	</main>
 	<footer>
